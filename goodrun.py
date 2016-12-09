3aa1976a0356c8cfb1d46398ee1b5fc5d1862f3d
noahMhorn <meganova879@gmail.com>
2016-12-09 09:41:59 -0500

    I swear it works this time

--- goodrun.py
+++ goodrun.py
@@ -9,4 +9,4 @@ arg.append(2)
 if conf.input_type == 1 :
 	os.system("./a.out " + str(arg[0]) + ' ' + str(arg[1]))
 if conf.input_type == 2 :
-	os.system("echo " + str(arg[0]) + " " + str(arg[1]) + " | ./a.out")
+	os.system("echo " + str(arg[0]) + "; echo " + str(arg[1]) + " | ./a.out")

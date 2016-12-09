d7132564a471c925f48cf04a8b097e90e614e12e
jsdsparky <jsdsparky@gmail.com>
2016-12-09 15:02:20 +0000

    errythang 8

--- debugGdb.py
+++ debugGdb.py
@@ -2,7 +2,7 @@ import gdb
 import sys
 str = input()
 gdb.execute('file ' + str)
-gdb.execute('b greet')
+gdb.execute('b greet') #should prolly not hardcode this need to integrate with jack and boot
 gdb.execute('r')
 gdb.execute('info frame')
 gdb.execute('quit')

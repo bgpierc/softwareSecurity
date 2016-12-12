3ac1208c7b32472ff7a36d34feec3e687d0e63ff
noahMhorn <meganova879@gmail.com>
2016-12-12 10:02:11 -0500

    Added goodrun.py

---
+++ goodrun.py
@@ -0,0 +1,12 @@
+import array
+from enum import Enum
+import os
+import config as conf
+def execute():
+	arg = []
+	arg = conf.args
+	if conf.input_type == 1 :
+		os.system("./a.out " + str(arg[0]) + ' ' + str(arg[1]))
+	if conf.input_type == 2 :
+		os.system("echo " + str(arg[0]) + "; echo " + str(arg[1]) + " | ./a.out")
+execute()
